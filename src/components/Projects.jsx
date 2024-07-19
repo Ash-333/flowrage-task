bee598fb2aa05f77dfa814577bd8406a67b40cc4
Ash-333 <akamat62@gmail.com>
2024-07-19 22:27:16 +0545

    added animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import ProjectItemCard from "./ProjectItemCard";
+import { motion } from "framer-motion"
 
 const Projects = () => {
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['repoData'],
+    queryKey: ["repoData"],
     queryFn: async () => {
       const response = await fetch(
-        'https://api.jsonbin.io/v3/b/669a0cc7ad19ca34f889b787',
-      )
-      return await response.json()
+        "https://api.jsonbin.io/v3/b/669a0cc7ad19ca34f889b787"
+      );
+      return await response.json();
     },
-  })
+  });
 
-  if (isPending) return 'Loading...'
+  if (isPending) return "Loading...";
 
-  if (error) return 'An error has occurred: ' + error.message
+  if (error) return "An error has occurred: " + error.message;
 
   return (
-    <div className="p-4">
+    <div className="p-4 w-screen">
       <h1 className="text-xl font-bold mb-8">My projects</h1>
-      <div className="grid grid-cols-4">
-      {
-        data.record.map((project)=>(
-          <ProjectItemCard title={project.title} image={project.thumbnail}/>
-        ))
-      }
+      <div className="grid grid-cols-4 items-center">
+        {data.record.map((project) => (
+          <motion.div
+            animate={{ x: 100 }}
+            transition={{ ease: "easeOut", duration: 2 }}
+          >
+            <ProjectItemCard title={project.title} image={project.thumbnail} />
+          </motion.div>
+        ))}
       </div>
     </div>
-  )
+  );
 };
 
 export default Projects;
